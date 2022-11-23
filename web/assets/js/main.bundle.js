447e8ddc5db0c489d7b3aea47a1c2fc1ee21febd
Jérôme Coupé <jercoupe@gmail.com>
2022-11-23 19:34:16 +0100

    includes, extends, scope and homepage fields

---
+++ web/assets/js/main.bundle.js
@@ -0,0 +1,1 @@
+(()=>{var e={body:document.querySelector("body"),menu:document.querySelector(".js-mobilemenu"),triggers:document.querySelectorAll(".js-menutrigger")},t={open:"is-open",active:"has-menu"},o=()=>{e.triggers.forEach(n=>{n.addEventListener("click",r=>{r.preventDefault(),e.menu.classList.toggle(t.open),e.body.classList.toggle(t.active)})})};o();})();
